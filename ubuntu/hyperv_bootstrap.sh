864f4ffcaeec52b1ba61639746db0c2bb1f64442
deoren <deoren@users.noreply.github.com>
2018-06-30 16:59:39 -0500

    Update Ubuntu Hyper-V bootstrap script
    - Dynamically pull Ubuntu release info
    - Split logic for package installs based on release codename
    - Install additional HWE tools packages if HWE kernel present

--- ubuntu/hyperv_bootstrap.sh
+++ ubuntu/hyperv_bootstrap.sh
@@ -7,6 +7,8 @@
 
 BASE_SRC_PATH="/tmp/config-files/ubuntu/all"
 
+UBUNTU_RELEASE="$(lsb_release -c | awk '{print $2}')"
+
 sudo apt-get update
 sudo apt-get install -y git
 
@@ -14,24 +16,49 @@ cd /tmp
 git clone https://github.com/deoren/config-files
 
 sudo cp -vf ${BASE_SRC_PATH}/apt/sources.list.template /etc/apt/
-sudo sed -i "s/CODENAME_PLACEHOLDER/$(lsb_release -c | awk '{print $2}')/g" /etc/apt/sources.list
+sudo sed -i "s/CODENAME_PLACEHOLDER/${UBUNTU_RELEASE}/g" /etc/apt/sources.list
 
+# Chosen mirrors default to IPv6, but attempts to use them usually fail
 sudo cp -vf ${BASE_SRC_PATH}/apt.conf.d/99force-ipv4 /etc/apt/apt.conf.d/
 
 sudo apt-get update
 
-# TODO: Check for Ubuntu 16.04. Install these packages. If newer, use next line
-sudo apt-get install \
-    linux-virtual-lts-xenial \
-    linux-tools-virtual-lts-xenial \
-    linux-cloud-tools-virtual-lts-xenial
+# Install "tools" packages to enable:
+#
+# VSS Snapshot daemon - This daemon is required to create live Linux virtual machine backups.
+# KVP daemon - This daemon allows setting and querying intrinsic and extrinsic key value pairs.
+# fcopy daemon - This daemon implements a file copying service between the host and guest.
 
-# sudo apt-get install \
-    # linux-image-virtual \
-    # linux-tools-virtual \
-    # linux-cloud-tools-virtual
+if [[ "$UBUNTU_RELEASE" == "xenial" ]]; then
+    sudo apt-get install \
+        linux-virtual-lts-xenial \
+        linux-tools-virtual-lts-xenial \
+        linux-cloud-tools-virtual-lts-xenial
+
+    # If a HWE kernel (newer than the base 4.4 or LTS kernel) is installed ...
+    #if dpkg -l | grep -E '^ii' | grep linux-image | grep -Eq '4.(10|13|15)'
+    if dpkg -l | grep -E '^ii' | grep linux-image | grep hwe
+    then
+
+        # Install matching "tools" packages to enable hv-fcopy-daemon operation
+        sudo apt-get install \
+            linux-virtual-hwe-16.04
+            linux-cloud-tools-virtual-hwe-16.04 \
+            linux-tools-virtual-hwe-16.04
+    fi
+
+else
+
+    # 17.04, 17.10, ...
+    sudo apt-get install \
+        linux-image-virtual \
+        linux-tools-virtual \
+        linux-cloud-tools-virtual
+
+fi
 
 
+# Override video resolution, tweak I/O settings, ...
 sudo cp -vf ${BASE_SRC_PATH}/etc/default/grub /etc/default/grub
 sudo update-grub
 
